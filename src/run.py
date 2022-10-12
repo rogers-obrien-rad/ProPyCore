19dc5252b7f20426f0affb3aa42546ea5d60e843
Hagen Fritz <hagenfritz@gmail.com>
2022-10-12 15:09:38 -0500

    redo for refactor (#4)

--- src/run.py
+++ src/run.py
@@ -9,25 +9,45 @@
 
 import argparse
 import pathlib
-from datetime import datetime
+import os
 
-from utils import logger, webpage
-from pycore import auth, companies, projects
+from datetime import datetime
+from dotenv import load_dotenv
+
+from propycore import procore
+from propycore.access import base
+from propycore.utils import logger
+
+# Load environment variables to populate Procore class
+if os.getenv("CLIENT_ID") is None:
+    load_dotenv()
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET= os.getenv("CLIENT_SECRET")
+REDIRECT_URI= os.getenv("REDIRECT_URI")
+OAUTH_URL= os.getenv("OAUTH_URL")
+BASE_URL= os.getenv("BASE_URL")
 
 PATH_TO_TOP = f"{pathlib.Path(__file__).resolve().parent.parent}"
 
 def main():
     """
-    
+    A display of the capabilities of the program
     """
     log = logger.setup("log", level="debug", stream=True)
     log.info(f"Started at {datetime.now()}")
-    auth_code = auth.get_auth_code()
-    access, created = auth.get_token(auth_code)
 
-
-    _ = companies.get(access)
-    _ = projects.get_projects(access)
+    connection = procore.Procore(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=REDIRECT_URI,
+        oauth_url=OAUTH_URL,
+        base_url=BASE_URL
+    )
+    company_list = connection.__companies__.get()
+    company_test = company_list[0]["id"]
+    project_test = connection.__projects__.get(company_id=company_test)
+    print(project_test)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
