8f5ecb87fee52cb210c7c37131055f3307fdef74
Hagen Fritz <hagenfritz@gmail.com>
2022-10-13 16:39:53 -0500

    wrap tests in class

--- tests/integration/test_procore_functionality.py
+++ tests/integration/test_procore_functionality.py
@@ -1,48 +1,28 @@
 import os
-from dotenv import load_dotenv
 
 from src.propycore import procore
 
-# Load environment variables to populate Procore class
-if os.getenv("CLIENT_ID") is None:
-    load_dotenv()
+class TestFunctionality:
 
-CLIENT_ID = os.getenv("CLIENT_ID")
-CLIENT_SECRET= os.getenv("CLIENT_SECRET")
-REDIRECT_URI= os.getenv("REDIRECT_URI")
-OAUTH_URL= os.getenv("OAUTH_URL")
-BASE_URL= os.getenv("BASE_URL")
+    def __init__(self) -> None:
 
-def test_connection():
-    connection = procore.Procore(
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        redirect_uri=REDIRECT_URI,
-        oauth_url=OAUTH_URL,
-        base_url=BASE_URL
-    )
-    assert connection is not None
+        self.connection = procore.Procore(
+            client_id=os.getenv("TEST_CLIENT_ID"),
+            client_secret=os.getenv("TEST_CLIENT_SECRET"),
+            redirect_uri=os.getenv("TEST_REDIRECT_URI"),
+            oauth_url=os.getenv("TEST_OAUTH_URL"),
+            base_url=os.getenv("TEST_BASE_URL")
+        )
 
-def test_company_listing():
-    connection = procore.Procore(
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        redirect_uri=REDIRECT_URI,
-        oauth_url=OAUTH_URL,
-        base_url=BASE_URL
-    )
-    temp_company_list = connection.__companies__.get()
-    assert temp_company_list is list
+    def test_connection(self):
+        assert self.connection is not None
 
-def test_project_listing():
-    connection = procore.Procore(
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        redirect_uri=REDIRECT_URI,
-        oauth_url=OAUTH_URL,
-        base_url=BASE_URL
-    )
-    company_list = connection.__companies__.get()
-    temp_company = company_list[0]
-    temp_projects = connection.__projects__.get(company_id=temp_company)
-    assert temp_projects is list
+    def test_company_listing(self):
+        temp_company_list = self.connection.__companies__.get()
+        assert temp_company_list is list
+
+    def test_project_listing(self):
+        company_list = self.connection.__companies__.get()
+        temp_company = company_list[0]
+        temp_projects = self.connection.__projects__.get(company_id=temp_company)
+        assert temp_projects is list
