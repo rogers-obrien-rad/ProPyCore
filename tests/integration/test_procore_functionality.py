adcde902bf951358341f6073af8de16ef36cd515
Hagen Fritz <hagenfritz@gmail.com>
2022-10-13 17:13:55 -0500

    append path

--- tests/integration/test_procore_functionality.py
+++ tests/integration/test_procore_functionality.py
@@ -1,4 +1,6 @@
 import os
+import sys
+sys.path.insert(0, '../../')
 
 from propycore import procore
 
--- tests/unit/test_snippets.py
+++ tests/unit/test_snippets.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.insert(0, '../../')
+
 from snippets import auth
 
 class TestAuth:
