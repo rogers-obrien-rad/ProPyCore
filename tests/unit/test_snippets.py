21cd16225e282ffe06d4af6e714bb177314fcbca
Hagen Fritz <hagenfritz@gmail.com>
2022-10-13 17:41:30 -0500

    change assertion type

--- tests/unit/test_snippets.py
+++ tests/unit/test_snippets.py
@@ -7,9 +7,9 @@ class TestAuth:
 
     def test_get_auth_code(self):
         auth_code = auth.get_auth_code()
-        assert auth_code is str
+        assert auth_code is not None
 
     def test_get_token(self):
         auth_code = auth.get_auth_code()
         access_token, _ = auth.get_token(auth_code)
-        assert access_token is str
+        assert access_token is not None
