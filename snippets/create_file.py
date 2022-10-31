595d41eb1f14cdad6b3733683009af9817faf22c
Hagen Fritz <hagenfritz@gmail.com>
2022-10-31 09:54:03 -0500

    add try/except

--- snippets/create_file.py
+++ snippets/create_file.py
@@ -4,6 +4,7 @@ import pathlib
 sys.path.append(f"{pathlib.Path(__file__).resolve().parent.parent}")
 
 from propycore.procore import Procore
+from propycore.exceptions import ProcoreException
 
 from dotenv import load_dotenv
 
@@ -25,25 +26,31 @@ if __name__ == "__main__":
 
     # Example 1: Create file in Root (no parent_id provided)
     # ---------
-    file_in_root = connection.__files__.create(
-        company_id=company["id"],
-        project_id=project["id"],
-        filepath=f"{pathlib.Path(__file__).resolve().parent.parent}/data/test/test_pdf.pdf"
-    )
-    print(f"{file_in_root['id']}: {file_in_root['name']}")
+    try:
+        file_in_root = connection.__files__.create(
+            company_id=company["id"],
+            project_id=project["id"],
+            filepath=f"{pathlib.Path(__file__).resolve().parent.parent}/data/test/test_pdf.pdf"
+        )
+        print(f"{file_in_root['id']}: {file_in_root['name']}")
+    except ProcoreException:
+        print("Example 1: File already exists in root")
 
     # Example 2: Create file in specified location
     # ---------
-    folder = connection.find_doc(
-        company_id=company["id"],
-        project_id=project["id"],
-        name="Subcontractors Orientation" # this needs to be a folder in your procore project
-    )
+    try:
+        folder = connection.find_doc(
+            company_id=company["id"],
+            project_id=project["id"],
+            name="Subcontractors Orientation" # this needs to be a folder in your procore project
+        )
 
-    file = connection.__files__.create(
-        company_id=company["id"],
-        project_id=project["id"],
-        parent_id=folder["id"],
-        filepath=f"{pathlib.Path(__file__).resolve().parent.parent}/data/test/test_pdf.pdf"
-    )
-    print(f"{file['id']}: {file['name']}")
+        file = connection.__files__.create(
+            company_id=company["id"],
+            project_id=project["id"],
+            parent_id=folder["id"],
+            filepath=f"{pathlib.Path(__file__).resolve().parent.parent}/data/test/test_pdf.pdf"
+        )
+        print(f"{file['id']}: {file['name']}")
+    except ProcoreException:
+        print("Example 2: File already exists in specified folder")
