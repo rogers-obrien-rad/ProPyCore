2b31f6e6b1ecdc104b37faf8516acc436fe5d4b3
Hagen Fritz <hagenfritz@gmail.com>
2022-11-02 11:56:54 -0500

    include export()

---
+++ snippets/export_rfis.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import pathlib
+sys.path.append(f"{pathlib.Path(__file__).resolve().parent.parent}")
+
+from propycore.procore import Procore
+from propycore.exceptions import NotFoundItemError
+
+from dotenv import load_dotenv
+import json
+
+if os.getenv("CLIENT_ID") is None:
+    load_dotenv()
+
+if __name__ == "__main__":
+    connection = Procore(
+        client_id=os.getenv("CLIENT_ID"),
+        client_secret=os.getenv("CLIENT_SECRET"),
+        redirect_uri=os.getenv("REDIRECT_URI"),
+        oauth_url=os.getenv("OAUTH_URL"),
+        base_url=os.getenv("BASE_URL")
+    )
+
+    company = connection.find_company(identifier="DataPull")
+    project = connection.find_project(company_id=company["id"], identifier="Sandbox Test Project")
+
+    # Example 1: Find rfi by number
+    # ---------
+    data = connection.__rfis__.export(
+        company_id=company["id"],
+        project_id=project["id"],
+    )
+    print(data)
