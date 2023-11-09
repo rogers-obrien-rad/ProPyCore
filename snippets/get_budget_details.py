f84f8f110b3008db7f598cd3e320e27d8e7ad602
Hagen Fritz <hfritz@r-o.com>
2023-11-09 17:07:40 -0600

    feat: add sample code for budgets

---
+++ snippets/get_budget_details.py
@@ -0,0 +1,80 @@
+import os
+import sys
+import pathlib
+sys.path.append(f"{pathlib.Path(__file__).resolve().parent.parent}")
+
+from propycore.procore import Procore
+
+from dotenv import load_dotenv
+import json
+
+if os.getenv("CLIENT_ID") is None:
+    load_dotenv()
+
+if __name__ == "__main__":
+    connection = Procore(
+        client_id=os.getenv("CLIENT_ID"),
+        client_secret=os.getenv("CLIENT_SECRET"),
+        redirect_uri=os.getenv("REDIRECT_URI"),
+        oauth_url=os.getenv("OAUTH_URL"),
+        base_url=os.getenv("BASE_URL")
+    )
+
+    company = connection.__companies__.find(identifier="Rogers-O`Brien Construction")
+    project = connection.__projects__.find(
+        company_id=company["id"],
+        identifier="Sandbox Test Project"
+    )
+    budget_view = connection.__budgets__.find_view(
+        company_id=company["id"],
+        project_id=project["id"],
+        identifier=414205
+    )
+
+    # Example 1: List all columns
+    # ---------
+    print("Example 1")
+    budget_columns = connection.__budgets__.get_budget_columns(
+        company_id=company["id"],
+        project_id=project["id"],
+        budget_view_id=budget_view["id"]
+    )
+
+    print(f"Number of Budget Columns: {len(budget_columns)}")
+
+    # Example 2: Find column by name
+    # ---------
+    print("Example 2")
+    column_by_name = connection.__budgets__.find_budget_column(
+        company_id=company["id"],
+        project_id=project["id"],
+        budget_view_id=budget_view["id"],
+        identifier="Cost to Date"
+    )
+
+    print(json.dumps(column_by_name, indent=4))
+
+    # Example 3: List all rows
+    # ---------
+    print("Example 3")
+    budget_rows = connection.__budgets__.get_budget_rows(
+        company_id=company["id"],
+        project_id=project["id"],
+        budget_view_id=budget_view["id"]
+    )
+
+    print(f"Number of Budget Rows: {len(budget_rows)}")
+
+    # Example 4: Find row by name
+    # ---------
+    print("Example 4")
+    row_by_name = connection.__budgets__.find_budget_row(
+        company_id=company["id"],
+        project_id=project["id"],
+        budget_view_id=budget_view["id"],
+        identifier="None"
+    )
+
+    print(json.dumps(row_by_name, indent=4))
+
+    
