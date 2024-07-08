cfce4dc51ce24b5936a859d8046fb3ad6335a5ca
Hagen Fritz <hfritz@r-o.com>
2024-07-08 12:47:31 -0500

    docs: update version number

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requirements = [
 
 setup(
     name="ProPyCore",
-    version="0.0.2",
+    version="0.0.4",
     author="Hagen E. Fritz",
     author_email="hfritz@r-o.com",
     description="Interact with Procore through Python for data connection applications",
