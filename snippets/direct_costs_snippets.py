b80e6b960f4ca169172015995b177daf586a0271
Hagen Fritz <hfritz@r-o.com>
2024-07-31 14:19:19 -0500

    feat: add example for post method

--- snippets/direct_costs_snippets.py
+++ snippets/direct_costs_snippets.py
@@ -1,14 +1,25 @@
 import os
+import json
 import sys
+import random
 import pathlib
 sys.path.append(f"{pathlib.Path(__file__).resolve().parent.parent}")
 
 from ProPyCore.procore import Procore
 from dotenv import load_dotenv
-import json
+from datetime import datetime, timedelta
 
 if os.getenv("CLIENT_ID") is None:
     load_dotenv()
+
+def random_date(start, end):
+    """
+    This function will return a random datetime between two datetime
+    objects.
+    """
+    return start + timedelta(
+        seconds=random.randint(0, int((end - start).total_seconds())),
+    )
 
 if __name__ == "__main__":
     connection = Procore(
@@ -78,24 +89,48 @@ if __name__ == "__main__":
 
     # Example 4
     # ---------
+    print("Example 4")
     # Example of creating a Direct Cost item
+    # Generate a random date between two dates
+    start_date = datetime.strptime('2024-01-01', '%Y-%m-%d')
+    end_date = datetime.strptime('2024-12-31', '%Y-%m-%d')
+    random_day = random_date(start_date, end_date).date()
     direct_cost_data = {
-        "description": "Invoice for April",
-        "direct_cost_date": "2024-12-14",
+        "description": f"Invoice for {random_day.strftime('%B %d, %Y')}",
+        "direct_cost_date": random_day.strftime('%Y-%m-%d'),
         "employee_id": 8780450,
-        "invoice_number": "Invoice # abc123",
-        "origin_data": "OD-2398273424",
-        "origin_id": "px-1990",
-        "payment_date": "2025-01-10",
-        "received_date": "2025-01-08",
+        "invoice_number": f"Invoice {random_day.strftime('%Y-%m-%d')}",
+        "origin_data": f"OD-{''.join(random.choices('0123456789', k=10))}",
+        "origin_id": f"px-{''.join(random.choices('0123456789', k=4))}",
+        "payment_date": f"{(start_date + timedelta(days=10)).strftime('%Y-%m-%d')}",
+        "received_date": f"{(start_date + timedelta(days=10)).strftime('%Y-%m-%d')}",
         "status": "approved",
         "terms": "Net 50",
         "vendor_id": 5181441,
         "direct_cost_type": "invoice"
     }
 
+    line_items = [
+        {
+            "manual_amount": 1000,
+            "wbs_code_id": 1989,
+            "description": "100' of Copper Piping",
+            "direct_cost_id": 81753,
+            "origin_data": "OD-2398273424",
+            "origin_id": "px-1990",
+            "quantity": 82.0201,
+            "ref": "PQRS5678",
+            "unit_cost": 12.03,
+            "uom": "cubic feet"
+        }
+    ]
+
+    attachments = ["./dummy/direct_costs_module.pdf"]
+
     created_direct_cost = connection.direct_costs.create(
         company_id=company["id"],
         project_id=project["id"],
         direct_cost_data=direct_cost_data,
+        line_items=line_items,
+        attachments=attachments
     )
