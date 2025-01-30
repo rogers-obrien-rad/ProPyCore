44056fcca4727384a3dd6869edc25dee4f835dbb
Hagen Fritz <hfritz@r-o.com>
2025-01-30 10:15:23 -0600

    feat: change post request to accept json instead of data

--- ProPyCore/access/base.py
+++ ProPyCore/access/base.py
@@ -91,13 +91,19 @@ class Base:
 
         # Make the request with file if necessary
         if files is None:
-            response = requests.post(
+            headers["Content-Type"] = "application/json"
+            response = requests.request(
+                "POST",
                 url,
                 headers=headers,
-                data=data 
+                json=data  # Use json parameter instead of data to properly serialize
             )
+            print(f"Request URL: {response.request.url}")
+            print(f"Request Headers: {response.request.headers}")
+            print(f"Request Data: {response.request.body}")
         elif data is None:
-            response = requests.post(
+            response = requests.request(
+                "POST",
                 url,
                 headers=headers,
                 files=files  # use files for multipart/form-data
